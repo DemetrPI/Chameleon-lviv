247c723809214e8d561136f29558a46558a96adb
DemetrPI <demetriy.78@gmail.com>
2024-03-16 20:58:07 +0100

    Removed marker from map

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,5 +1,5 @@
-import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
-import { useMemo } from "react";
+import { Wrapper } from "@googlemaps/react-wrapper";
+import { GoogleMap } from "@react-google-maps/api";
 import { ApiKey } from "../utils/googleMapAPIKey";
 
 const containerStyle = {
@@ -16,40 +16,27 @@ const containerStyle = {
   alignItems: "center",
 };
 
-const Map = () => {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey:process.env.REACT_APP_GOOGLE_API_KEY,
-  });
-
-  const center = useMemo(
-    () => ({ lat: 49.795556361046614, lng: 24.051039025841092 }),
-    []
-  );
-
-  return (
-    <div className="Map">
-      {!isLoaded ? (
-        <h1>Loading...</h1>
-      ) : (
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={18}
-          mapId={"mapId"}
-        >
-          <Marker position={{
-            lat:49.79555636104661,
-            lng: 24.051039025841092
-
-          }}
-          icon={"https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"}
-          />
-        </GoogleMap>
-      )}
-    </div>
-  );
+const center = {
+  lat: 49.795556361046614,
+  lng: 24.051039025841092,
 };
 
+function Map() {
+
+
+  
+  return (
+    <Wrapper apiKey={ApiKey}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={18}
+        mapId={"mapId"}
+      >
+
+      </GoogleMap>
+    </Wrapper>
+  );
+}
+
 export default Map;
-
-
